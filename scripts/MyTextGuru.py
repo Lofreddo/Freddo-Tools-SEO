59d60eaa2e3aa44add0511888492a6e7037ec1c2
Lofreddo <a.weecxsteen29@gmail.com>
2024-08-08 15:46:58 +0200

    Update MyTextGuru.py

--- scripts/MyTextGuru.py
+++ scripts/MyTextGuru.py
@@ -1,96 +1,94 @@
+import streamlit as st
 import pandas as pd
 import nltk
 from nltk import ngrams
 from bs4 import BeautifulSoup
 from collections import Counter
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import CountVectorizer
 import treetaggerwrapper
 import string
 
-# Téléchargement de la liste de stop words et de 'punkt'
-nltk.download('stopwords')
-nltk.download('punkt')
+def app():
+    # Téléchargement de la liste de stop words et de 'punkt'
+    nltk.download('stopwords')
+    nltk.download('punkt')
 
-# Votre ami a suggéré d'utiliser cette liste de mots d'arrêt
-french_stopwords_list = nltk.corpus.stopwords.words('french')
-# Votre ami a suggéré d'utiliser cette liste de lettres de l'alphabet
-alphabet_list = list(string.ascii_lowercase)
+    # Votre ami a suggéré d'utiliser cette liste de mots d'arrêt
+    french_stopwords_list = nltk.corpus.stopwords.words('french')
+    # Votre ami a suggéré d'utiliser cette liste de lettres de l'alphabet
+    alphabet_list = list(string.ascii_lowercase)
 
-# Ouvrir le fichier Excel et lire la colonne de texte
-df = pd.read_excel('test-top-words.xlsx')
-html_content = df['hn_content'].tolist()
+    # Interface Streamlit pour charger le fichier Excel
+    uploaded_file = st.file_uploader("Choisissez un fichier Excel", type="xlsx")
+    if uploaded_file is not None:
+        df = pd.read_excel(uploaded_file)
+        html_content = df['hn_content'].tolist()
 
-# Fonction pour nettoyer le texte HTML
+        # Fonction pour nettoyer le texte HTML
+        def clean_html(text):
+            if isinstance(text, str):
+                soup = BeautifulSoup(text, "html.parser")
+                return soup.get_text().lower()
+            else:
+                return ""
 
+        # Nettoyer le texte HTML
+        cleaned_text = [clean_html(text) for text in html_content]
 
-def clean_html(text):
-    if isinstance(text, str):
-        soup = BeautifulSoup(text, "html.parser")
-        return soup.get_text().lower()
-    else:
-        return ""
+        tagger = treetaggerwrapper.TreeTagger(TAGDIR='C:/TreeTagger', TAGLANG='fr')
 
+        # Fonction pour extraire les mots et les n-grams
+        def extract_words_ngrams(text, n):
+            tokens = nltk.word_tokenize(text)
+            pos_tokens = treetaggerwrapper.make_tags(tagger.tag_text(text))
 
-# Nettoyer le texte HTML
-cleaned_text = [clean_html(text) for text in html_content]
+            # Ignore les mots vides, non alphabétiques, les verbes, les auxiliaires et les lettres seules
+            filtered_tokens = []
+            for token, pos_token in zip(tokens, pos_tokens):
+                if token not in french_stopwords_list and token.isalpha() and isinstance(pos_token, treetaggerwrapper.Tag) and not (pos_token.pos.startswith('VER') or pos_token.pos.startswith('AUX')) and token not in alphabet_list:
+                    filtered_tokens.append(token)
+            
+            if n > 1:
+                n_grams = ngrams(filtered_tokens, n)
+                return [' '.join(grams) for grams in n_grams]
+            else:
+                return filtered_tokens
 
-tagger = treetaggerwrapper.TreeTagger(TAGDIR='C:/TreeTagger', TAGLANG='fr')
+        # Nombre de mots/n-grams que vous voulez pour chaque colonne
+        num_words = st.number_input('Nombre de mots les plus courants à extraire', value=50)
+        num_bigrams = st.number_input('Nombre de bigrammes les plus courants à extraire', value=30)
+        num_trigrams = st.number_input('Nombre de trigrammes les plus courants à extraire', value=30)
 
-# Fonction pour extraire les mots et les n-grams
+        # Extraire les mots et les n-grams
+        words = []
+        bigrams = []
+        trigrams = []
+        for text in cleaned_text:
+            words.extend(extract_words_ngrams(text, 1))
+            bigrams.extend(extract_words_ngrams(text, 2))
+            trigrams.extend(extract_words_ngrams(text, 3))
 
+        # Compter les occurrences
+        words_counter = Counter(words)
+        bigrams_counter = Counter(bigrams)
+        trigrams_counter = Counter(trigrams)
 
-def extract_words_ngrams(text, n):
-    tokens = nltk.word_tokenize(text)
-    pos_tokens = treetaggerwrapper.make_tags(tagger.tag_text(text))
+        # Prendre les mots/n-grams les plus courants
+        most_common_words = [word for word, count in words_counter.most_common(num_words)]
+        most_common_bigrams = [bigram for bigram, count in bigrams_counter.most_common(num_bigrams)]
+        most_common_trigrams = [trigram for trigram, count in trigrams_counter.most_common(num_trigrams)]
 
-    # Ignore les mots vides, non alphabétiques, les verbes, les auxiliaires et les lettres seules
-    filtered_tokens = []
-    for token, pos_token in zip(tokens, pos_tokens):
-        if token not in french_stopwords_list and token.isalpha() and isinstance(pos_token, treetaggerwrapper.Tag) and not (pos_token.pos.startswith('VER') or pos_token.pos.startswith('AUX')) and token not in alphabet_list:
-            filtered_tokens.append(token)
-        else:
-            print(
-                f"Token filtré : {token}, Partie du discours : {pos_token.pos if isinstance(pos_token, treetaggerwrapper.Tag) else 'Not a Tag'}")
+        # Afficher les résultats dans Streamlit
+        st.write("Mots les plus courants:", ', '.join(most_common_words))
+        st.write("Bigrammes les plus courants:", ', '.join(most_common_bigrams))
+        st.write("Trigrammes les plus courants:", ', '.join(most_common_trigrams))
 
-    if n > 1:
-        n_grams = ngrams(filtered_tokens, n)
-        return [' '.join(grams) for grams in n_grams]
-    else:
-        return filtered_tokens
+        # Ajouter les mots/n-grams les plus courants au DataFrame
+        df['Mots les plus courants'] = ', '.join(most_common_words)
+        df['Bigrammes les plus courants'] = ', '.join(most_common_bigrams)
+        df['Trigrammes les plus courants'] = ', '.join(most_common_trigrams)
 
-
-# Nombre de mots/n-grams que vous voulez pour chaque colonne
-num_words = 50
-num_bigrams = 30
-num_trigrams = 30
-
-# Extraire les mots et les n-grams
-words = []
-bigrams = []
-trigrams = []
-for text in cleaned_text:
-    words.extend(extract_words_ngrams(text, 1))
-    bigrams.extend(extract_words_ngrams(text, 2))
-    trigrams.extend(extract_words_ngrams(text, 3))
-
-# Compter les occurrences
-words_counter = Counter(words)
-bigrams_counter = Counter(bigrams)
-trigrams_counter = Counter(trigrams)
-
-# Prendre les mots/n-grams les plus courants
-most_common_words = [word for word,
-                     count in words_counter.most_common(num_words)]
-most_common_bigrams = [bigram for bigram,
-                       count in bigrams_counter.most_common(num_bigrams)]
-most_common_trigrams = [trigram for trigram,
-                        count in trigrams_counter.most_common(num_trigrams)]
-
-# Ajouter les mots/n-grams les plus courants au DataFrame
-df['Mots les plus courants'] = ', '.join(most_common_words)
-df['Bigrammes les plus courants'] = ', '.join(most_common_bigrams)
-df['Trigrammes les plus courants'] = ', '.join(most_common_trigrams)
-
-# Enregistrer le DataFrame dans un nouveau fichier Excel
-df.to_excel('nouveau_fichier.xlsx', index=False)
+        # Enregistrer le DataFrame dans un nouveau fichier Excel
+        df.to_excel('nouveau_fichier.xlsx', index=False)
+        st.write("Le fichier a été enregistré sous le nom 'nouveau_fichier.xlsx'. Vous pouvez le télécharger en utilisant le lien ci-dessous.")
+        st.download_button(label="Télécharger le fichier Excel", data=df.to_excel(index=False), file_name='nouveau_fichier.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
