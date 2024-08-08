cea1751320d529d2593aa5c464e4883a503bd8d6
Lofreddo <a.weecxsteen29@gmail.com>
2024-08-08 23:07:54 +0200

    Update Extract_Serp.py

--- scripts/Extract_Serp.py
+++ scripts/Extract_Serp.py
@@ -11,7 +11,7 @@ st.title("Recherche de mots-clés avec ValueSERP")
 keywords_input = st.text_area("Entrez vos mots-clés, un par ligne:")
 
 # Conversion des mots-clés en liste
-keywords = [keyword.strip() for keyword in keywords_input.strip().split('\n') if keyword.strip()]
+keywords = keywords_input.strip().split('\n')
 
 # Menu déroulant pour sélectionner le domaine Google
 google_domain = st.selectbox(
@@ -40,13 +40,12 @@ def fetch_results(keyword):
         'output': 'csv',
         'csv_fields': 'search.q,organic_results.position,organic_results.title,organic_results.link,organic_results.domain,organic_results.page'
     }
-    try:
-        api_result = requests.get('https://api.valueserp.com/search', params)
-        api_result.raise_for_status()  # Raise an error for bad status codes
+    api_result = requests.get('https://api.valueserp.com/search', params)
+    if api_result.status_code == 200:
         result_df = pd.read_csv(io.StringIO(api_result.text))
         return result_df
-    except requests.RequestException as e:
-        st.error(f"La requête pour le mot-clé '{keyword}' a échoué: {e}")
+    else:
+        st.error(f"La requête pour le mot-clé '{keyword}' a échoué avec le code d'état {api_result.status_code}.")
         return None
 
 # Bouton pour lancer la recherche
