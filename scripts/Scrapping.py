e9e4f086d1a66abb26a6edf3bd1ab269749b2be1
Lofreddo <a.weecxsteen29@gmail.com>
2024-08-13 00:47:41 +0200

    Update Scrapping.py

--- scripts/Scrapping.py
+++ scripts/Scrapping.py
@@ -12,18 +12,21 @@ def clean_html_content(soup):
         tag.unwrap()
     for tag in soup.find_all(['tr', 'td', 'table', 'button']):
         tag.decompose()
+
     social_keywords = ['facebook', 'twitter', 'instagram', 'linkedin', 'youtube', 'social']
-    for a_tag in soup.find_all('a', href=True):  # Ajout de 'href=True' pour filtrer les balises <a> sans href
-        href_value = a_tag.get('href')
-        if isinstance(href_value, str) and any(keyword in href_value.lower() for keyword in social_keywords):
+    for a_tag in soup.find_all('a', href=True):
+        if any(keyword in a_tag['href'].lower() for keyword in social_keywords):
             a_tag.decompose()
         else:
             a_tag.unwrap()
+
     for tag in soup.find_all(True):
         tag.attrs = {}
+
     for tag in soup.find_all():
         if not tag.get_text(strip=True):
             tag.decompose()
+
     return soup
 
 # Fonction pour extraire le contenu et la structure des balises hn de manière asynchrone
@@ -41,22 +44,26 @@ async def get_hn_and_content(session, url):
     soup = BeautifulSoup(html, 'html.parser')
     for tag in soup.find_all(['header', 'footer']):
         tag.decompose()
+
     clean_soup = clean_html_content(soup)
     html_content = ""
     structure_hn = []
+    
     for tag in clean_soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'ul', 'li', 'ol']):
         if tag.get_text(strip=True):
             html_content += str(tag) + '\n'
         if tag.name.startswith('h'):
             structure_hn.append(f"<{tag.name}>{tag.get_text(strip=True)}</{tag.name}>")
+
     structure_hn_str = "\n".join(structure_hn)
     html_content = re.sub(r'\t+', ' ', html_content)
     html_content = re.sub(' +', ' ', html_content)
     html_content = "\n".join([line for line in html_content.splitlines() if line.strip()])
+
     return html_content.strip(), structure_hn_str
 
 # Fonction pour traiter les URLs de manière asynchrone
-async def process_urls_async(urls, max_concurrent_requests=300):  # Augmentation à 300 requêtes simultanées
+async def process_urls_async(urls, max_concurrent_requests=300):
     async with aiohttp.ClientSession() as session:
         semaphore = asyncio.Semaphore(max_concurrent_requests)
 
