0dde72da50bf4d6b41e7ca60aa560004bd32594b
Lofreddo <a.weecxsteen29@gmail.com>
2024-08-15 17:09:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scripts import MyTextGuru, MyTextGuruBulk, ExtractSerps, MasterSpinGenerator, Scrapping, ExtractSerpsV2, PointsChauds, DomainExpirationChecker
+from scripts import MyTextGuru, MyTextGuruBulk, ExtractSerps, MasterSpinGenerator, Scrapping, ExtractSerpsV2, PointsChauds, DomaineExpirationChecker
 
 # Configuration des pages
 PAGES = {
@@ -10,7 +10,7 @@ PAGES = {
     "MasterSpinGenerator": MasterSpinGenerator,
     "Scrapping": Scrapping,
     "PointsChauds": PointsChauds,
-    "Domain Expiration Checker": DomainExpirationChecker  # Ajout du nouveau script ici
+    "Domain Expiration Checker": DomaineExpirationChecker  # Ajout du nouveau script ici
 }
 
 # Créer une sidebar pour la navigation
